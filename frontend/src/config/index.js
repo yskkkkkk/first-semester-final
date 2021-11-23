8bac7b3ec94647f5b27a49fd6c2a2ddc3374f2fc
fbeotjd <fb1014@naver.com>
2021-11-23 13:57:19 +0900

    Refactor: server port(8080), web port(80)

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost/";
+const API_BASE_URL = "http://localhost:8080/";
 const APT_DEAL_URL =
   "http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev";
 
